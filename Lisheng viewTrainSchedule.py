a9ffdffe64726f8286595b04f96ae70d66487613
lisheng0316 <lisheng0316@gmail.com>
2016-04-08 23:08:33 -0400

    Added files via upload

--- Lisheng viewTrainSchedule.py
+++ Lisheng viewTrainSchedule.py
@@ -1,10 +1,16 @@
-    def viewTrainScheduleClicked(self):
-        self.functionWindow.withdraw()
-        self.viewTrainScheduleWindow = Tk()
-        self.viewTrainScheduleWindow.title("Train Sales System")
-        self.buildViewTrainScheduleWindow(self.viewTrainScheduleWindow)
-        self.viewTrainScheduleWindow.Toplevel()
+    import ttk
+    from tkinter import *
 
+    def viewTrainScheduleClicked(self):
+
+        self.createViewTrainScheduleWindow()
+        self.buildViewTrainScheduleWindow(self.viewTrainScheduleWindow)
+        self.functionWindow.withdraw()
+
+
+    def createViewTrainScheduleWindow(self):
+        self.viewTrainScheduleWindow = Toplevel()
+        self.viewTrainScheduleWindow.title("Train Sales System")
 
     def buildViewTrainScheduleWindow(self,viewTrainScheduleWindow):
         # Title Label
@@ -25,12 +31,47 @@
 
 
     def searchButtonClicked(self):
-        self.viewTrainScheduleWindow.withdraw()
-        self.viewTrainScheduleWindow2 = Tk()
-        self.viewTrainScheduleWindow2.title("Train Sales System")
-        self.buildViewTrainScheduleWindow2(self.viewTrainScheduleWindow)
-        self.viewTrainScheduleWindow2.Toplevel()
 
+        self.createViewTrainScheduleWindow2
+        self.buildViewTrainScheduleWindow2(self.viewTrainScheduleWindow2)
+
+        self.viewTrainScheduleWindow.withdraw()
+
+    def createViewTrainScheduleWindow2(self):
+        self.viewTrainScheduleWindow2 = Toplevel()
+        self.viewTrainScheduleWindow2.title("Train Sales System")
+
+    def bulidViewTrainScheduleWindow2(self,viewTrainScheduleWindow2):
+        viewTrainScheduleLabel = Label(viewTrainScheduleWindow2, text= "View Train Schedule")
+        viewTrainScheduleLabel.grid(row=1, column=1, sticky=W+E)
+
+
+        viewTrainScheduleLabel = Label(viewTrainScheduleWindow2, text="Schedule TABLE")
+        viewTrainScheduleLabel.grid(row=2, column=1, sticky=W+E)
+
+        #root = Tk()
+
+        # tree = ttk.Treeview(root, column=("first", "second", "third", "fourth"))
+
+        # tree.column("first", width = 100, anchor = "center")
+        # tree.column("second", width = 100, anchor = "center")
+        # tree.column("third", width = 100, anchor = "center")
+        # tree.column("fourth", width = 100, anchor = "center")
+        # tree.heading("first", text = "Train")
+        # tree.heading("second", text = "Arrival Time")
+        # tree.heading("third", text = "Departure Time")
+        # tree.heading("fourth", text = "Station")
+        # tree.pack()
+        #viewTrainScheduleTree.insert("first", text = "Train")
+
+        backButton = Button(viewTrainScheduleWindow2, text="Back", command=self.backButtonClicked)
+        backButton.grid(row=3, column=1)
+
+    def backButtonClicked(self):
+
+        self.createViewTrainScheduleWindow
+        self.buildViewTrainScheduleWindow(self.viewTrainScheduleWindow)
+        self.viewTrainScheduleWindow2.withdraw()
 
 
 
