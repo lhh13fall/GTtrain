e658b61143dbd03a1b1691559d57dd85f196a031
lhh13fall <lhh13fall@users.noreply.github.com>
2016-04-16 01:01:01 -0400

    Update Train ticket.py

--- Train ticket.py
+++ Train ticket.py
@@ -313,7 +313,6 @@ class GTTrain:
         # Click Log Out Buttion on Choose Functionality Window:
         # Destroy Choose Functionality Window
         # Display Login Window
-        self.db.close()
         self.chooseFunctionalityWindow.destroy()
         self.loginWindow.deiconify()
 
@@ -1292,9 +1291,30 @@ class GTTrain:
 
         #View Review Label
         viewReviewLabel = Label(viewReviewWindow2,text="View Review")
-        viewReviewLabel.grid(row=1, column=2, sticky=W+E)
+        viewReviewLabel.grid(row=1, column=1, sticky=W+E, columnspan=3)
 
-        reviewTuple = self.cursor.execute("SELECT Rating, Comment FROM Review WHERE TrainNum = %s", self.viewReviewTrainNumber)
+        self.cursor.execute("SELECT Rating, Comment FROM Review WHERE TrainNum = %s", self.viewReviewTrainNumber)
+        reviewTuple = self.cursor.fetchall()
+
+        ratingLabel = Label(viewReviewWindow2, text = "Rate")
+        ratingLabel.grid(row=2, column=1, sticky=W)
+        commentLabel = Label(viewReviewWindow2, text = "Comment")
+        commentLabel.grid(row=2, column=2, sticky=W)
+        i = 2
+        
+        for review in reviewTuple:
+            print(review)
+            print(review[0])
+            print(review[1])
+            i = i+1
+            rating = Label(viewReviewWindow2, text = review[0])
+            rating.grid(row=i, column=1)
+            comment = Label(viewReviewWindow2, text = review[1])
+            comment.grid(row=i, column=2)
+
+        
+
+
         # #View Review Treeview
         # viewReviewTree = ttk.Treeview(viewReviewWindow2, column=("1", "2"),height=2)
         # viewReviewTree.column("1", width = 150, anchor = "center")
@@ -1307,9 +1327,9 @@ class GTTrain:
 
         # viewReviewTree.grid(row=2,column=1,columnspan=3)
 
-        # #Back to Choose Functionality Button
-        # backToChooseFunctionalityButton = Button(viewReviewWindow2, text="Back to Choose Functionality", command = self.viewReviewWindow2BackToChooseFunctionalityButtonClicked)
-        # backToChooseFunctionalityButton.grid(row=3,column=2,sticky=W+E)
+        #Back to Choose Functionality Button
+        backToChooseFunctionalityButton = Button(viewReviewWindow2, text="Back to Choose Functionality", command = self.viewReviewWindow2BackToChooseFunctionalityButtonClicked)
+        backToChooseFunctionalityButton.grid(row=i+1,column=1,columnspan=2,sticky=W+E)
 
     def viewReviewWindow2BackToChooseFunctionalityButtonClicked(self):
         self.viewReviewWindow2.destroy()
@@ -1430,7 +1450,6 @@ class GTTrain:
     def chooseFunctionalityWindowManagerLogOutButtonClicked(self):
         # When click logOutButton in chooseFunctionalityWindow,
         # it will destroy chooseFunctionalityWindow and display Login Window
-        self.db.close()
         self.chooseFunctionalityWindowManager.destroy()
         self.loginWindow.deiconify()
 
